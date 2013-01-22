f3794308be240eafa119ccbfb2c9bb8d970e7090
Brad Rogers <rogers907@gmail.com>
2013-01-22 08:23:29 -0700

    commiting all files

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// js/app.js
+
+var app = app || {};
+var ENTER_KEY = 13;
+
+$(function() {
+
+	// Kick things off by creating the **App**.
+	console.log("attempting to create the view AppView!");
+	new app.AppView();
+
+});
---
+++ js/routers/todo-router.js
@@ -0,0 +1,22 @@
+// js/routers/routers.js
+
+// Todo Router
+// -----------
+
+var Workspace = Backbone.Router.extend({
+	routes:{
+		'*filter': 'setFilter'
+	},
+
+	setFilter: function( param ) {
+		// Set the current filter to be used
+		window.app.TodoFilter = param.trim() || '';
+
+		// Trigger a collection filter event, causing hiding/unhiding
+		// of Todo view items
+		window.app.Todos.trigger('filter');
+	}
+});
+
+app.TodoRouter = new Workspace();
+Backbone.history.start();
